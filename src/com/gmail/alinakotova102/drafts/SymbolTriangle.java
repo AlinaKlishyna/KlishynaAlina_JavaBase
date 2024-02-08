bd02599ef9b6b1b84973177a4e775a93d9be3fad
Anonimka <alinakotova102@gmail.com>
2024-02-08 13:26:29 +0100

    Created method createTriangleWhile()

--- src/com/gmail/alinakotova102/drafts/SymbolTriangle.java
+++ src/com/gmail/alinakotova102/drafts/SymbolTriangle.java
@@ -2,25 +2,50 @@ package com.gmail.alinakotova102.drafts;
 
 public class SymbolTriangle {
     static String symbol = "+";
+
     public static void main(String[] args) {
-        createTriangleFor(7);
+        //createTriangleFor(3);
+        //createTriangleWhile(7);
     }
 
-    public static void createTriangleFor(int height){
-            int countSymbols = 1;
-            int distance =height;
-            for (int i = 0; i < height; i++){
-                for (int l = distance; l>0; l--){
-                    System.out.print(" ");
-                }
+    public static void createTriangleFor(int height) {
+        int countSymbols = 1;
+        int distance = height;
+        for (int i = 0; i < height; i++) {
+            for (int d = distance; d > 0; d--) {
+                System.out.print(" ");
+            }
 
-                for (int j = 0; j < countSymbols; j++){
-                    System.out.print(symbol);
-                }
-                System.out.println();
+            for (int j = 0; j < countSymbols; j++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+            countSymbols += 2;
+            distance--;
+        }
+    }
 
-                countSymbols+=2;
+    public static void createTriangleWhile(int height) {
+        int countSymbols = 1;
+        int counter = 0;
+        int j = 0;
+        int d = 0;
+        int distance = height;
+        while (counter < height) {
+            while (distance > d) {
+                System.out.print(" ");
                 distance--;
             }
+            while (j < countSymbols) {
+                System.out.print(symbol);
+                j++;
+            }
+            countSymbols += 2;
+            j = 0;
+            System.out.println();
+            counter++;
+            distance = height - counter;
+
         }
+    }
 }
