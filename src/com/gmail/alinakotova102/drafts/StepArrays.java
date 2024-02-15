ee43a2f0b53a86c98140582d5547bcf29aa12dda
Anonimka <alinakotova102@gmail.com>
2024-02-15 17:29:32 +0100

    Created method sortEvenOdd()

--- src/com/gmail/alinakotova102/drafts/StepArrays.java
+++ src/com/gmail/alinakotova102/drafts/StepArrays.java
@@ -6,23 +6,55 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class StepArrays {
     public static void main(String[] args) {
+        System.out.println("Creation of a stepped array with random numbers");
+        System.out.print("Enter N - the number of lines: ");
         int countRows = enterNumber();
+        System.out.print("Enter M - the maximum number of elements in a row: ");
         int maxCount = enterNumber();
-        int[][] numbers = createArray(countRows, maxCount);
-        displayConsole(numbers);
+
+        int[][] array = createArray(countRows, maxCount);
+        System.out.println("\nInitial array");
+        displayConsole(array);
+
+        System.out.println("\nModified array");
+        int[][] arraySorted = sortEvenOdd(array);
+        displayConsole(arraySorted);
     }
 
-    public static void displayConsole(int[][] array){
-        for (int[] element: array){
-            System.out.print(Arrays.toString(element));
+    public static int[][] sortEvenOdd(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length - 1; j++) {
+                for (int k = 0; k < array[i].length - i; k++) {
+                    if ((i + 1) % 2 == 0) {
+                        while (array[i][k] > array[i][j + 1]) {
+                            int saveNumber = array[i][j + 1];
+                            array[i][j + 1] = array[i][k];
+                            array[i][k] = saveNumber;
+                        }
+                    } else {
+                        while (array[i][k] < array[i][j + 1]) {
+                            int saveNumber = array[i][j + 1];
+                            array[i][j + 1] = array[i][k];
+                            array[i][k] = saveNumber;
+                        }
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void displayConsole(int[][] array) {
+        for (int[] element : array) {
+            System.out.print(Arrays.toString(element) + "\n");
         }
         System.out.println();
     }
 
-    public static int[][] createArray(int length, int countInRow){
+    public static int[][] createArray(int length, int countInRow) {
         int[][] numbersRandom = new int[length][countInRow];
-        for (int i =0; i < numbersRandom.length; i++){
-            for (int j =0; j<numbersRandom[i].length; j++){
+        for (int i = 0; i < numbersRandom.length; i++) {
+            for (int j = 0; j < numbersRandom[i].length; j++) {
                 numbersRandom[i][j] = ThreadLocalRandom.current().nextInt(0, countInRow);
             }
         }
@@ -34,15 +66,15 @@ public class StepArrays {
         while (true) {
             if (scanner.hasNextInt()) {
                 int number = scanner.nextInt();
-                if (number > 0 && number < 999) {
+                if (number >= 0 && number < 999) {
                     return number;
                 } else {
                     System.out.print("Enter a number between [1-999]: ");
                 }
             } else {
                 System.out.print("Value non valid! Enter again: ");
-                scanner.nextLine();
             }
+            scanner.nextLine();
         }
     }
 }
