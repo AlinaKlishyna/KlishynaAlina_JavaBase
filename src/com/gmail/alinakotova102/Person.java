409005ef6616a328cc3f80195c2706e61bcc5081
Anonimka <alinakotova102@gmail.com>
2024-02-17 00:31:42 +0100

    Modified variable lastname -> lastName

--- src/com/gmail/alinakotova102/Person.java
+++ src/com/gmail/alinakotova102/Person.java
@@ -1,36 +1,36 @@
-package com.gmail.alinakotova102.drafts;
+package com.gmail.alinakotova102;
 
 public class Person {
     String name;
-    String lastname;
+    String lastName;
     String city;
     long phone;
 
     public static void main(String[] args) {
         Person willSmithUser = new Person();
         willSmithUser.name = "Will";
-        willSmithUser.lastname = "Smith";
+        willSmithUser.lastName = "Smith";
         willSmithUser.city = "New York";
         willSmithUser.phone = 2936729462846L;
         System.out.println(willSmithUser.personInfo());
 
         Person jackieChan = new Person();
         jackieChan.name = "Jackie";
-        jackieChan.lastname = "Chan";
+        jackieChan.lastName = "Chan";
         jackieChan.city = "Shanghai";
         jackieChan.phone = 12312412412L;
         System.out.println(jackieChan.personInfo());
 
         Person sherlockHolmes = new Person();
         sherlockHolmes.name = "Sherlock";
-        sherlockHolmes.lastname = "Holmes";
+        sherlockHolmes.lastName = "Holmes";
         sherlockHolmes.city = "London";
         sherlockHolmes.phone = 37742123513L;
         System.out.println(sherlockHolmes.personInfo());
     }
 
     public String personInfo() {
-        return "Зателефонувати громадянину " + name + " " + lastname + " із міста " + city + " можна за номером "
+        return "Зателефонувати громадянину " + name + " " + lastName + " із міста " + city + " можна за номером "
                 + phone + "\n";
     }
 }
