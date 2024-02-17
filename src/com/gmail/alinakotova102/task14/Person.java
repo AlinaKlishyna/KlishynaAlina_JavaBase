de171d2633bb8977d87f0b3c4d0324ad4d416d71
Anonimka <alinakotova102@gmail.com>
2024-02-17 01:26:13 +0100

    Added class PersonRunner
    Method main() in class was modified
    
    In class Person added getters and setters. Modified method personInfo()

---
+++ src/com/gmail/alinakotova102/task14/Person.java
@@ -0,0 +1,54 @@
+package com.gmail.alinakotova102.task14;
+
+public class Person {
+    private String name;
+    private String lastName;
+    private String city;
+    private long phone;
+
+    public Person() {}
+    public Person(String name, String lastName, String city, long phone) {
+        this.name = name;
+        this.lastName = lastName;
+        this.city = city;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String personInfo(String name, String lastName, String city, long phone) {
+        return "Зателефонувати громадянину " + (this.name=name) + " " + (this.lastName =lastName) + " із міста " +
+                (this.city=city) + " можна за номером " + (this.phone = phone) + "\n";
+    }
+}
+
