82e48a2f7b18148ccd676c443add8d805f679f91
Anonimka <alinakotova102@gmail.com>
2024-02-17 01:42:51 +0100

    Reformatted Code

--- src/com/gmail/alinakotova102/task14/Person.java
+++ src/com/gmail/alinakotova102/task14/Person.java
@@ -9,13 +9,6 @@ public class Person {
     public Person() {
     }
 
-    public Person(String name, String lastName, String city, long phone) {
-        this.name = name;
-        this.lastName = lastName;
-        this.city = city;
-        this.phone = phone;
-    }
-
     public String getName() {
         return name;
     }
--- src/com/gmail/alinakotova102/task14/PersonRunner.java
+++ src/com/gmail/alinakotova102/task14/PersonRunner.java
@@ -3,12 +3,14 @@ package com.gmail.alinakotova102.task14;
 public class PersonRunner {
     public static void main(String[] args) {
         Person willSmithUser = new Person();
-        System.out.println(willSmithUser.personInfo("Will", "Smith", "New York", 2936729462846L));
+        System.out.println(willSmithUser.personInfo("Will", "Smith", "New York",
+                2936729462846L));
 
         Person jackieChan = new Person();
         System.out.println(jackieChan.personInfo("Jackie", "Chan", "Shanghai", 12312412412L));
 
         Person sherlockHolmes = new Person();
-        System.out.println(sherlockHolmes.personInfo("Sherlock", "Holmes", "London", 37742123513L));
+        System.out.println(sherlockHolmes.personInfo("Sherlock", "Holmes", "London",
+                37742123513L));
     }
 }
