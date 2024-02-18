2d102c8677ad488120847714e4609c580b378131
Anonimka <alinakotova102@gmail.com>
2024-02-18 19:18:42 +0100

    Modified constructors

--- src/com/gmail/alinakotova102/task15/Burger.java
+++ src/com/gmail/alinakotova102/task15/Burger.java
@@ -1,55 +1,42 @@
 package com.gmail.alinakotova102.task15;
 
 public class Burger {
-    private static String bun = "bun";
+    private String bun;
     private String meat;
     private int countMeat;
-    private static String cheese = "cheese";
-    private static String herbs = "herbs";
-    private static String mayonnaise = "mayonnaise";
+    private String cheese;
+    private String herbs;
+    private String sauce;
 
-    public static String getBun() {
-        return bun;
+    public Burger() {
+        this.bun = "bun";
+        this.cheese = "cheese";
+        this.herbs = "herbs";
+        this.sauce = "mayonnaise";
     }
 
-    public static String getCheese() {
-        return cheese;
-    }
-
-    public static String getHerbs() {
-        return herbs;
-    }
-
-    public static String getMayonnaise() {
-        return mayonnaise;
-    }
-
-    public Burger(String bun, String meat, int countMeat, String cheese, String herbs, String mayonnaise) {
-        Burger.bun = bun;
+    public Burger(String meat, int countMeat, String sauce) {
+        this();
         this.meat = meat;
         this.countMeat = countMeat;
-        Burger.cheese = cheese;
-        Burger.herbs = herbs;
-        Burger.mayonnaise = mayonnaise;
-        info(bun, meat, countMeat, cheese, herbs, mayonnaise);
+        this.sauce = sauce;
+        info(bun, meat, countMeat, cheese, herbs, sauce);
     }
 
-    public Burger(String bun, String meat, int countMeat, String cheese, String herbs) {
-        Burger.bun = bun;
+    public Burger(String meat, int countMeat) {
+        this();
         this.meat = meat;
         this.countMeat = countMeat;
-        Burger.cheese = cheese;
-        Burger.herbs = herbs;
         info(bun, meat, countMeat, cheese, herbs);
     }
 
-    public void info(String bun, String meat, int countMeat, String cheese, String herbs, String mayonnaise) {
+    public void info(String bun, String meat, int countMeat, String cheese, String herbs, String sauce) {
         if (countMeat == 1) {
             System.out.printf("Normal burger\nIngredients: %s, %s(count x%d), %s, %s, %s\n\n", bun, meat, countMeat,
-                    cheese, herbs, mayonnaise);
+                    cheese, herbs, sauce);
         } else {
             System.out.printf("Normal burger with double meat\nIngredients: %s, %s(count x%d), %s, %s, %s\n\n", bun,
-                    meat, countMeat, cheese, herbs, mayonnaise);
+                    meat, countMeat, cheese, herbs, sauce);
         }
     }
 
--- src/com/gmail/alinakotova102/task15/BurgerMain.java
+++ src/com/gmail/alinakotova102/task15/BurgerMain.java
@@ -2,8 +2,8 @@ package com.gmail.alinakotova102.task15;
 
 public class BurgerMain {
     public static void main(String[] args) {
-       Burger orderFirst = new Burger(Burger.getBun(), "chicken", 1, Burger.getCheese(), Burger.getHerbs(), Burger.getMayonnaise());
-       Burger orderSecond = new Burger(Burger.getBun(), "pork", 2, Burger.getCheese(), Burger.getHerbs(), Burger.getMayonnaise());
-       Burger orderThird = new Burger(Burger.getBun(), "chicken", 1, Burger.getCheese(), Burger.getHerbs());
+       Burger orderFirst = new Burger("chicken", 1, "mayonnaise");
+       Burger orderSecond = new Burger("pork", 2, "mayonnaise");
+       Burger orderThird = new Burger("chicken", 1);
     }
 }
