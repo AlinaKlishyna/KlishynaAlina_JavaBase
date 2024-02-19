e88945a6e64dbb7a291908d9e427f80eb9eee45c
Anonimka <alinakotova102@gmail.com>
2024-02-19 16:58:51 +0100

    Crested class FitnessTracker

---
+++ src/com/gmail/alinakotova102/task16/FitnessTracker.java
@@ -0,0 +1,15 @@
+package com.gmail.alinakotova102.task16;
+
+public class FitnessTracker {
+    private String name;
+    private byte dayOfBirthday;
+    private byte monthOfBirthday;
+    private short yearOfBirthday;
+    private String email;
+    private long phone;
+
+    String lastname;
+    int weight;
+    int pressure;
+    int stepsOneDay;
+}
