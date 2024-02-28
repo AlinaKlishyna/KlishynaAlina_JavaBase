817715031d459d5fc08af9701a1977a1dede1098
AlinaKlishyna <alinakotova102@gmail.com>
2024-02-28 20:00:43 +0100

    Renamed variables

--- src/com/gmail/alinakotova102/task19/PointRunner.java
+++ src/com/gmail/alinakotova102/task19/PointRunner.java
@@ -2,32 +2,32 @@ package com.gmail.alinakotova102.task19;
 
 public class PointRunner {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Point pointA = new Point(3, 5);
-        pointA.currentCoordinates("A");
-        System.out.println("Point A. Get x = " + pointA.getX());
+        Point start = new Point(3, 5);
+        start.currentCoordinates("start");
+        System.out.println("Point start. Get x = " + start.getX());
 
-        Point pointB = new Point(12, 5);
-        pointB.currentCoordinates("B");
+        Point middle = new Point(12, 5);
+        middle.currentCoordinates("middle");
 
         System.out.println("\nDistance: ");
-        System.out.println(pointA.getDistance(pointB));
+        System.out.println(start.getDistance(middle));
         Point another = new Point(0,0);
-        System.out.println(pointA.getDistance(another, pointB));
+        System.out.println(start.getDistance(another, middle));
 
         System.out.println("\nCompare: ");
-        System.out.println("A " + pointA.toString());
-        System.out.println("B " + pointB.toString());
-        System.out.println("Point A == Point B: " + pointA.equals(pointB));
-        System.out.println("Point B == Point A: " + pointB.equals(pointA));
-        pointA.setX(12);
-        System.out.println("A " + pointA.toString());
-        System.out.println("Point A == Point B: " + pointA.equals(pointB));
-        System.out.println("Point A == null: " + pointA.equals(null));
+        System.out.println("start " + start.toString());
+        System.out.println("middle " + middle.toString());
+        System.out.println("Point start == Point middle: " + start.equals(middle));
+        System.out.println("Point middle == Point start: " + middle.equals(start));
+        start.setX(12);
+        System.out.println("start " + start.toString());
+        System.out.println("Point start == Point middle: " + start.equals(middle));
+        System.out.println("Point start == null: " + start.equals(null));
 
-        Point pointC = pointA.clone();
-        System.out.println("C " + pointC.toString());
-        System.out.println("Point A == Point C: " + pointA.equals(pointC));
-        System.out.println("\nHash code A: " + pointA.hashCode() + "\nHash code B: " + pointB.hashCode() +
-                "\nHash code C: " + pointC.hashCode());
+        Point end = start.clone();
+        System.out.println("end " + end.toString());
+        System.out.println("Point start == Point end: " + start.equals(end));
+        System.out.println("\nHash code start: " + start.hashCode() + "\nHash code middle: " + middle.hashCode() +
+                "\nHash code end: " + end.hashCode());
     }
 }
