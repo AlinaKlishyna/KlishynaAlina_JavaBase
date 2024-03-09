90c6ead8d582de65422095379534f3869aacf08e
Alina Klishyna <alinakotova102@gmail.com>
2024-03-09 11:55:28 +0100

    Added maven

---
+++ src/test/java/com/gmail/alinakotova102/task20/ArraysDemoTest.java
@@ -0,0 +1,37 @@
+package com.gmail.alinakotova102.task20;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ArraysDemoTest {
+    public static void main(String[] args) {
+       runAllTests();
+    }
+
+    public static void runAllTests(){
+        ArraysDemoTest arraysDemoTest = new ArraysDemoTest();
+        arraysDemoTest.averageTest();
+        arraysDemoTest.averageZeroTest();
+    }
+
+    @Test
+    void averageTest(){
+        int[] array = {2, 4, 6, 8};
+        double actual = Arrays.average(array);
+
+        double expected = 5;
+        System.out.println("Actual value: " + actual + " - Expected: " + expected);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void averageZeroTest(){
+        int[] arr = {};
+        double actual = Arrays.average(arr);
+
+        double expected = 0.0;
+        System.out.println("Actual value: " + actual + " - Expected: " + expected);
+        Assertions.assertEquals(expected, actual);
+    }
+}
