e2da6234cc03d5717b2aedaf6face01451facc64
AlinaKlishyna <alinakotova102@gmail.com>
2024-03-12 17:08:53 +0100

    Modified method square()

--- src/main/java/com/gmail/alinakotova102/task20/Arrays.java
+++ src/main/java/com/gmail/alinakotova102/task20/Arrays.java
@@ -1,9 +1,7 @@
 package com.gmail.alinakotova102.task20;
 
 public class Arrays {
-    public static void main(String[] args) {
 
-    }
     public static double average(int[] array) {
         if (array == null || array.length == 0) {
             return 0.0;
@@ -16,23 +14,19 @@ public class Arrays {
     }
 
     public static boolean square(int[][] array) {
+        if (array == null) {
+            return false;
+        }
         int j = 0;
-        if (!(array == null)) {
-            if (!(array.length == 0)) {
-                for (int i = 0; i < array.length; i++) {
-                    for (int k = 0; k < array[i].length; k++) {
-                        if (!(array[i] == null || array[i].length == 0)) {
-                            if (array.length != array[i].length) {
-                                j++;
-                            }
-                        }
-                        if (j == 0) {
-                            return true;
-                        }
-                    }
+        for (int i = 0; i < array.length; i++) {
+            if (!(array[i] == null || array[i].length == 0)) {
+                if (array.length == array[i].length) {
+                    j++;
                 }
-
             }
+        }
+        if (j == array[0].length & j == array.length) {
+            return true;
         }
         return false;
     }
