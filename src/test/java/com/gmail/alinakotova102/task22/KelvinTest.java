21ead2752f96e2561fbd0a5539ec85149f25c5af
AlinaKlishyna <alinakotova102@gmail.com>
2024-03-17 22:43:58 +0100

    Added new test methods for KelvinTest

--- src/test/java/com/gmail/alinakotova102/task22/KelvinTest.java
+++ src/test/java/com/gmail/alinakotova102/task22/KelvinTest.java
@@ -3,10 +3,10 @@ package com.gmail.alinakotova102.task22;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KelvinTest {
-    final double PRECISION = 0.00001;
+    final double PRECISION = 0.0001;
     Kelvin kelvin;
 
     @BeforeEach
@@ -28,6 +28,42 @@ class KelvinTest {
         double actual = kelvin.temperatureFromCelsius(-10);
 
         double expected = 263.15;
+
+        assertEquals(expected, actual, PRECISION);
+    }
+
+    @Test
+    void temperatureFromCelsiusZero() {
+        double actual = kelvin.temperatureFromCelsius(0);
+
+        double expected = 273.15;
+
+        assertEquals(expected, actual, PRECISION);
+    }
+
+    @Test
+    void temperatureToCelsius() {
+        double actual = kelvin.temperatureToCelsius(10);
+
+        double expected = -263.15;
+
+        assertEquals(expected, actual, PRECISION);
+    }
+
+    @Test
+    void temperatureToCelsiusNegative() {
+        double actual = kelvin.temperatureToCelsius(-5);
+
+        double expected = -278.15;
+
+        assertEquals(expected, actual, PRECISION);
+    }
+
+    @Test
+    void temperatureToCelsiusZero() {
+        double actual = kelvin.temperatureToCelsius(0);
+
+        double expected = -273.15;
 
         assertEquals(expected, actual, PRECISION);
     }
