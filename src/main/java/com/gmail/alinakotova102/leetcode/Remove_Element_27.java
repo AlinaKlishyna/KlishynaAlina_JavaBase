80be90791f08a689a8143106dbacd889c0010861
AlinaKlishyna <alinakotova102@gmail.com>
2024-03-24 15:25:17 +0100

    Deleted method point of entry

--- src/main/java/com/gmail/alinakotova102/leetcode/Remove_Element_27.java
+++ src/main/java/com/gmail/alinakotova102/leetcode/Remove_Element_27.java
@@ -1,16 +1,6 @@
 package com.gmail.alinakotova102.leetcode;
 
-import java.util.Arrays;
-
 public class Remove_Element_27 {
-    public static void main(String[] args) {
-        Remove_Element_27 removeElement27 = new Remove_Element_27();
-        System.out.println(Arrays.toString(removeElement27.removeElement(new int[]{3, 2, 2, 3}, 3)));
-        System.out.println(Arrays.toString(removeElement27.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2)));
-        System.out.println(Arrays.toString(removeElement27.removeElement(new int[]{4,5}, 5)));
-
-        //System.out.println(removeElement27.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
-
 //        int[] nums = [...]; // Input array
 //        int val = ...; // Value to remove
 //        int[] expectedNums = [...]; // The expected answer with correct length.
@@ -22,7 +12,6 @@ public class Remove_Element_27 {
 //        sort(nums, 0, k); // Sort the first k elements of nums
 //        for (int i = 0; i < actualLength; i++) {
 //            assert nums[i] == expectedNums[i];
-    }
 
     public int[] removeElement(int[] nums, int val) {
         int k = 0;
