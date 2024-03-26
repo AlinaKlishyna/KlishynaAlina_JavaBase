6fc2a22d0b4a89c212861338051377dfbb02c315
AlinaKlishyna <alinakotova102@gmail.com>
2024-03-26 16:09:42 +0100

    Modified method

--- src/main/java/com/gmail/alinakotova102/leetcode/MergeSortedArray_88.java
+++ src/main/java/com/gmail/alinakotova102/leetcode/MergeSortedArray_88.java
@@ -3,28 +3,20 @@ package com.gmail.alinakotova102.leetcode;
 import java.util.Arrays;
 
 public class MergeSortedArray_88 {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(merge(new int[]{1,2,3}, 3, new int[]{2,5,6},3)));
-    }
     public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
-        int[]save = nums1;
-        nums1 = new int[m + n];
-        //объединение массив в nums1
-        for (int i = 0; i < m+n-1; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i < m) {
-                    nums1[i] = save[i];
-                } else {
-                    nums1[i++] = nums2[j];
-                }
-            }
+        int[] save = nums1;
+        nums1=new int[m+n];
+        if (m<n){
+            int val = n;
+            n=m;
+            m=val;
         }
-        //сортировка
-        for (int i = 0; i < nums1.length-1; i++) {
-            if (nums1[i] > nums1[i+1]){
-                int val = nums1[i];
-                nums1[i] = nums1[i+1];
-                nums1[i+1] = val;
+
+        for (int i =0; i<m+n;i++){
+            for (int j = 0; j < n; j++) {
+                if (save[j] > nums2[j]){
+                    nums1[i++]= save[j];
+                }
             }
         }
         return nums1;
