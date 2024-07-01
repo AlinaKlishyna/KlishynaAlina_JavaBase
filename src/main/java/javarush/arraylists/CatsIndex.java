ab6d99cf0cca02b38d325448eb5d5902e81711a9
Alina Klishyna <alinakotova102@gmail.com>
2024-07-01 21:09:26 +0200

    task with arraylist

---
+++ src/main/java/javarush/arraylists/CatsIndex.java
@@ -0,0 +1,17 @@
+package javarush.arraylists;
+
+import java.util.ArrayList;
+
+public class CatsIndex {
+    public static void main(String[] args) {
+        ArrayList<String> cats = new ArrayList<>();
+        cats.add("Барсик"); //индекс 0
+        cats.add("Фоксик");  //индекс 1
+        cats.add("Толик"); //индекс 2
+        cats.add("Геральт из Ривии"); //индекс 3
+        System.out.println(cats);
+        cats.remove(3);
+        System.out.println(cats);
+
+    }
+}
