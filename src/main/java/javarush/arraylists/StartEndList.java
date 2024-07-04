456bb9b37d38c3a6e070368232f09fca97ed89b4
Alina Klishyna <alinakotova102@gmail.com>
2024-07-04 16:09:57 +0200

    functional with funcArrayList()/funcLinkedList()

---
+++ src/main/java/javarush/arraylists/StartEndList.java
@@ -0,0 +1,25 @@
+package javarush.arraylists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class StartEndList {
+    public static void main(String[] args) {
+        //funcArrayList();
+        funcLinkedList();
+    }
+
+    public static void funcArrayList() {
+        ArrayList<String> line = new ArrayList<>();
+        line.add("1-ый");
+        line.add("2-ой"); //добавляется в конец списка
+        System.out.println(line);  // вывод: [1-ый, 2-ой]
+    }
+
+    public static void funcLinkedList() {
+        LinkedList<String> line = new LinkedList<>();
+        line.add("1-ый");
+        line.add("2-ой"); //добавляется в конец списка
+        System.out.println(line);  // вывод: [1-ый, 2-ой]
+    }
+}
