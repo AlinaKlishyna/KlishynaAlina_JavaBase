c7ff0ad3cc6b440c30d23520dccb1995a28b42b7
Alina <alinakotova102@gmail.com>
2025-04-07 20:05:42 +0200

    task about merge two arrays

--- src/main/java/stepik/array/mergeArray.java
+++ src/main/java/stepik/array/mergeArray.java
@@ -6,27 +6,30 @@ import java.util.Arrays;
 public class mergeArray {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(mergeArrays(new int[]{0, 2, 2}, new int[]{1, 3})));
+        System.out.println(Arrays.toString(mergeArrays(new int[]{41, 55, 61, 73}, new int[]{1, 15, 30, 40, 80})));
     }
 
     public static int[] mergeArrays(int[] a1, int[] a2) {
-        int[] res = new int[a1.length + a2.length];
-        int j =0;
-        for (int a : a1) {
-            res[j++] = a;
-        }
-        for (int a : a2) {
-            res[j++] = a;
-        }
-
-        for (int i = 0; i < res.length-1; i++) {
-            while (res[i]>res[i+1]) {
-                int save = res[i];
-                res[i] = res[i+1];
-                res[i+1] = save;
-                i=0;
+        int[] result = new int[a1.length + a2.length];
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < result.length; k++) {
+            if (i < a1.length && j < a2.length) {
+                if (a1[i] < a2[j]) {
+                    result[k] = a1[i];
+                    i++;
+                } else {
+                    result[k] = a2[j];
+                    j++;
+                }
+            } else if (i >= a1.length) {
+                result[k] = a2[j];
+                j++;
+            } else {
+                result[k] = a1[i];
+                i++;
             }
         }
-        return res;
+        return result;
     }
 }
