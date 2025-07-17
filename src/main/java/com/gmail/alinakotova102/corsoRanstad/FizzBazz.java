cdd3794142c968419a01be4567ee1b29d6bb5247
Alina <alinakotova102@gmail.com>
2025-07-17 12:45:23 +0200

    fizzbazz

---
+++ src/main/java/com/gmail/alinakotova102/corsoRanstad/FizzBazz.java
@@ -0,0 +1,20 @@
+package com.gmail.alinakotova102.corsoRanstad;
+
+public class FizzBazz {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 100) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println(i + " - FizzBazz");
+            else if (i % 3 == 0)
+                System.out.println(i + " - Fizz");
+            else if (i % 5 == 0)
+                System.out.println(i + " - Bazz");
+            else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
