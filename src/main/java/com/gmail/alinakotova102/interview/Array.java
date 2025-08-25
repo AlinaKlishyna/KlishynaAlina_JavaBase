b2d15b51afc8094e131e166f2ca58b695fb51d52
Alina <alinakotova102@gmail.com>
2025-08-25 19:21:05 +0200

    added example by leetcode

--- src/main/java/com/gmail/alinakotova102/interview/Array.java
+++ src/main/java/com/gmail/alinakotova102/interview/Array.java
@@ -7,6 +7,49 @@ public class Array {
         System.out.println("{3,7,0,2,0} --> " + Arrays.toString(moveZero(new int[]{3, 7, 0, 2, 0})));
         System.out.println("{3,7,0,2,0} sort bubble personal --> " + Arrays.toString(bubbleSortPersonal(new int[]{3, 7, 0, 2, 0})));
         System.out.println("{3,7,0,2,0} sort bubble --> " + Arrays.toString(bubbleSort(new int[]{3, 7, 0, 2, 0})));
+
+        //Input: nums = [3,6,1,0]
+        //Output: 1
+        //Explanation: 6 is the largest integer.
+        //For every other number in the array x, 6 is at least twice as big as x.
+        //The index of value 6 is 1, so we return 1.
+        System.out.println("{3,6,1,0} dominant index --> " + dominantIndex(new int[]{3,6,1,0}));
+
+        //Input: nums = [1,2,3,4]
+        //Output: -1
+        //Explanation: 4 is less than twice the value of 3, so we return -1.
+        System.out.println("{1,2,3,4} dominant index --> " + dominantIndex(new int[]{1,2,3,4}));
+        System.out.println("{1,0} dominant index --> " + dominantIndex(new int[]{1,0}));
+        System.out.println("{0,0,1,2} dominant index --> " + dominantIndex(new int[]{0,0,1,2}));
+    }
+
+
+    public static int dominantIndex(int[] nums) {
+        int max = nums[0];
+        for (int num : nums) {
+            if (num>max) {
+                max = num;
+            }
+        }
+        System.out.println("Max number: " + max);
+
+        int maxIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max == nums[i]) {
+                maxIndex = i;
+            }
+        }
+        System.out.println("Max Index: " + maxIndex);
+
+        boolean dominant = true;
+        for (int num : nums) {
+            if (num != max) {
+                if (num*2 > max) {
+                    dominant = false;
+                }
+            }
+        }
+        return dominant ? maxIndex : -1;
     }
 
     // перенести все 0 в конец и сохранить порядок чисел {3,7,0,2,0} -> {3,7,2,0,0}
