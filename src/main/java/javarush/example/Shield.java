b145c97f5c89418c92dfeec6c0fde1d64a9d1259
Alina <alinakotova102@gmail.com>
2025-09-30 10:19:31 +0200

    task shield

---
+++ src/main/java/javarush/example/Shield.java
@@ -0,0 +1,8 @@
+package javarush.example;
+
+public class Shield {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+    }
+}
